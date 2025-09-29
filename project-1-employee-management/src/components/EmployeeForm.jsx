c3f4f05101a70c42288aa4a0fe867859b782b6a0
Muhammad Asadullah <masdullah230@gmail.com>
2025-09-30 01:15:08 +0500

    devv

---
+++ project-1-employee-management/src/components/EmployeeForm.jsx
@@ -0,0 +1,34 @@
+export default function EmployeeForm({ formData, errors, onChange, onSubmit }) {
+  return (
+    <div className="absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.7)] flex justify-center items-center">
+      <div className="bg-white p-6 rounded-lg shadow-md mx-auto w-[280px]">
+        <h2 className="text-gray-700 font-semibold mb-4">Add Employee</h2>
+        <form onSubmit={onSubmit} className="space-y-4 text-[12px]">
+          <div>
+            <label className="text-gray-700 font-medium">Name</label>
+            <input type="text" name="name" value={formData.name} onChange={onChange} className="w-full mt-1 p-2 border border-gray-200 rounded focus:ring-2 focus:ring-green-600 bg-gray-100" />
+            {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name}</p>}
+          </div>
+          <div>
+            <label className="text-gray-700 font-medium">Email</label>
+            <input type="email" name="email" value={formData.email} onChange={onChange} className="w-full mt-1 p-2 border border-gray-200 rounded focus:ring-2 focus:ring-green-600 bg-gray-100" />
+            {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
+          </div>
+          <div>
+            <label className="text-gray-700 font-medium">Department</label>
+            <input type="text" name="department" value={formData.department} onChange={onChange} className="w-full mt-1 p-2 border border-gray-200 rounded focus:ring-2 focus:ring-green-600 bg-gray-100" />
+            {errors.department && <p className="text-red-500 text-sm mt-1">{errors.department}</p>}
+          </div>
+          <div>
+            <label className="text-gray-700 font-medium">Role</label>
+            <input type="text" name="role" value={formData.role} onChange={onChange} className="w-full mt-1 p-2 border border-gray-200 rounded focus:ring-2 focus:ring-green-600 bg-gray-100" />
+            {errors.role && <p className="text-red-500 text-sm mt-1">{errors.role}</p>}
+          </div>
+          <button type="submit" className="w-full bg-green-600 text-white py-2 rounded hover:bg-green-500 transition">
+            Add Employee
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
---
+++ project-1-employee-management/src/components/EmployeeTable.jsx
@@ -0,0 +1,34 @@
+export default function EmployeeTable({ employees, onDelete, onEdit }) {
+  return (
+    <div className='overflow-x-auto shadow-md rounded-sm'>
+      <table className='text-sm text-left text-gray-700'>
+        <thead className='text-[10px] uppercase bg-gray-200'>
+          <tr>
+            <th className='px-2 pl-4 py-3'>Name</th>
+            <th className='px-2 py-3'>Email</th>
+            <th className='px-2 py-3'>Department</th>
+            <th className='px-2 py-3'>Role</th>
+            <th className='px-2 py-3 text-center'>Delete</th>
+            <th className='px-2 py-3 text-center'>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id} className='border-t border-gray-200 text-[12px]'>
+              <td className='px-2 pl-4 py-3'>{employee.name}</td>
+              <td className='px-2 py-3'>{employee.email}</td>
+              <td className='px-2 py-3'>{employee.department}</td>
+              <td className='px-2 py-3'>{employee.role}</td>
+              <td className='px-2 py-2 text-center'>
+                <button onClick={() => onDelete(employee.id)} className='cursor-pointer uppercase text-[11px] font-bold text-center text-white bg-red-400 px-2 py-[2px] rounded-sm'>Delete</button>
+              </td>
+              <td className='px-2 py-2 text-center'>
+                <button onClick={() => onEdit(employee.id)} className='cursor-pointer uppercase text-[11px] font-bold text-center text-white bg-gray-500 px-2 py-[2px] rounded-sm'>Edit</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
